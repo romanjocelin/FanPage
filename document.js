002935edce991ad9a1b0b1e6fd45421a83b6759b
romanjocelin <jocelin.roman@speeracademy.org>
2016-11-28 18:36:20 -0600

    V.2.2.0.0 still wont work but divs are added to jq

--- document.js
+++ document.js
@@ -1,7 +1,14 @@
-$(document).ready(function() {
-    $('div').mouseenter(function() {
-          $('div').fadeTo('fast',1.00);
+$(document).ready(function(){
+    $('#col-md-6').click(function(){
+        $('#col-md-6').fadeOut('slow');
     });
-});
-        
-  
+
+    $('#button').click(function(){
+        var toAdd = $('input[name=checkListItem]').val();
+        $('.list').append('<div class="item">' + toAdd + "</div>")
+    });
+    $(document).on('click','.item',function(){
+        $(this).remove();
+    });
+
+
